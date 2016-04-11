3e7f839cf625c7665ae3ee549d85d1d0a9de7d8f
Felipe Menanteau <felipe@illinois.edu>
2016-04-11 20:36:09 +0000

    Updating setup.py file

--- setup.py
+++ setup.py
@@ -3,28 +3,16 @@ from distutils.core import setup
 
 # The main call
 setup(name='qatoolkit',
-      version ='0.2.1',
+      version ='0.2.3',
       license = "GPL",
       description = "A set of handy Python utility scripts for DESDM",
-      author = "Felipe Menanteau, Robert Gruendl, Ricardo Covarrubias",
-      author_email = "felipe@illinois.edu",
+      author = "Robert Gruendl",
+      author_email = "gruendl@illinois.edu",
       packages = ['qatoolkit'],
       package_dir = {'': 'python'},
       scripts = ['bin/assess_SE_products.py',
                  'bin/check_flats.py',
-                 'bin/color_tile',
-                 'bin/compare_corners_NewFramework',
-                 'bin/display_CoaddDES',
-                 'bin/display_DECam_MEFs',
-                 'bin/display_DECam_detection_NOMAD',
-                 'bin/flats_analysis.py',
-                 'bin/make_png_focus_chips_SQL',
-                 'bin/make_png_focus_chips_bypath',
-                 'bin/projectDECamPNG',
-                 'bin/projectDECamPNG_SQL',
-                 'bin/projectDECamPNG_bypath'],
-      data_files=[('ups',['ups/qatoolkit.table']),
-                  ('etc',['etc/default.stiff','etc/default.swarp'])]
-      )           
+                 'bin/flats_analysis.py']
+      data_files=[('ups',['ups/qatoolkit.table'])])           
                  
 
