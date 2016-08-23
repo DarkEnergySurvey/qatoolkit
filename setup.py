647977185c69c59946d941556e9915f8067fdcc7
daues <daues>
2016-08-23 02:36:52 +0000

    ready trunk for tag 0.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 # The main call
 setup(name='qatoolkit',
-      version ='0.2.4',
+      version ='0.2.6',
       license = "GPL",
       description = "A set of handy Python utility scripts for DESDM",
       author = "Robert Gruendl",
